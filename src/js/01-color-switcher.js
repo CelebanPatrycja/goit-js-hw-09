be7782762c6f4f3dbbf4ff6c76138090f8cb5f39
CelebanPatrycja <114691633+CelebanPatrycja@users.noreply.github.com>
2023-07-05 21:25:06 +0200

    update hw9

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', () => {
+  const colorSwitch = () => {
+    body.style.backgroundColor = getRandomHexColor();
+  };
+  timerId = setInterval(() => {
+    console.log('to jest zmiana koloru:', colorSwitch());
+  }, 1000);
+  btnStop.disabled = false;
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+  console.log('stop:', clearInterval(timerId));
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
